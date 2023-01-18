1ee4e73c20b542c0f4096f6eeba8734b4a3689b5
Marc Hembach <marchembach1@gmx.de>
2023-01-18 16:33:09 +0100

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,8 +26,8 @@ const RESOURCES = {
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "74b75dff20864ac3b11660fa474f062f",
-"/": "74b75dff20864ac3b11660fa474f062f",
+"index.html": "224301d3f20473c092997f043ea812a1",
+"/": "224301d3f20473c092997f043ea812a1",
 "main.dart.js": "f7d9eb746e4d822a3d8da8841ac97712",
 "manifest.json": "03f960253ff105704ac15cb60bb594ec",
 "version.json": "1ac57eb0214a8f5216f1da7dd479b33f"
